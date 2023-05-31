a80bd1c42e4a97e6b3e964157d895fe2773a8b2b
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-31 17:36:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-window.promises = [];
+// Array of promises
+const promises = [
+  new Promise((resolve) => setTimeout(() => resolve('Promise 1 resolved'), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 2 resolved'), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 4 resolved'), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 5 resolved'), getRandomTime()))
+];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Get a random time between 1 and 5 seconds
+function getRandomTime() {
+  return Math.floor(Math.random() * 5000) + 1000;
+}
+
+// Wait for the first promise to resolve using Promise.any
+Promise.any(promises)
+  .then((result) => {
+    // Print the result to the output div
+    document.getElementById('output').innerText = result;
+  });
